cde006d2670e8f35ae429ee50b8aecab6659852e
daile1807kaka@gmail.com <daile1807kaka@gmail.com>
2024-03-20 08:35:51 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -24,18 +24,6 @@ class Form1(Form1Template):
         self.insertion_sort(sorted_arr)
         self.box_InsertionSort.text = "Insertion Sort: " + ' '.join(map(str, sorted_arr))
 
-        sorted_arr = arr.copy()
-        self.selection_sort(sorted_arr)
-        self.box_SelectionSort.text = "Selection Sort: " + ' '.join(map(str, sorted_arr))
-
-        sorted_arr = arr.copy()
-        self.bubble_sort(sorted_arr)
-        self.box_BubbleSort.text = "Bubble Sort: " + ' '.join(map(str, sorted_arr))
-
-        sorted_arr = arr.copy()
-        self.merge_sort(sorted_arr)
-        self.box_MergeSort.text = "Merge Sort: " + ' '.join(map(str, sorted_arr))
-
     # Hàm sắp xếp chèn
     def insertion_sort(self, arr):
         for i in range(1, len(arr)):
@@ -45,54 +33,3 @@ class Form1(Form1Template):
                 arr[j + 1] = arr[j]
                 j -= 1
             arr[j + 1] = key
-    
-    
-    # Hàm sắp xếp lựa chọn
-    def selection_sort(self, arr):
-        for i in range(len(arr)):
-            min_index = i
-            for j in range(i + 1, len(arr)):
-                if arr[j] < arr[min_index]:
-                    min_index = j
-            arr[i], arr[min_index] = arr[min_index], arr[i]
-    
-    
-    # Hàm sắp xếp nổi bọt
-    def bubble_sort(self, arr):
-        n = len(arr)
-        for i in range(n - 1):
-            for j in range(0, n - i - 1):
-                if arr[j] > arr[j + 1]:
-                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    
-    
-    # Hàm sắp xếp trộn
-    def merge_sort(self, arr):
-        if len(arr) > 1:
-            mid = len(arr) // 2
-            left_half = arr[:mid]
-            right_half = arr[mid:]
-    
-            self.merge_sort(left_half)
-            self.merge_sort(right_half)
-    
-            i = j = k = 0
-    
-            while i < len(left_half) and j < len(right_half):
-                if left_half[i] < right_half[j]:
-                    arr[k] = left_half[i]
-                    i += 1
-                else:
-                    arr[k] = right_half[j]
-                    j += 1
-                k += 1
-    
-            while i < len(left_half):
-                arr[k] = left_half[i]
-                i += 1
-                k += 1
-    
-            while j < len(right_half):
-                arr[k] = right_half[j]
-                j += 1
-                k += 1
